4aac9760a69fbb1a616827235f807297f950c7ed
Andrew Moss <andrew@m0ss.dev>
2019-07-23 19:12:20 -0600

    Sankey online, TODO label and color

--- src/components/PlotlySankey/index.js
+++ src/components/PlotlySankey/index.js
@@ -1,54 +1,156 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-import sankeyData from './data';
 import realData from './realData';
+
+import sankeyData from './sankeyData';
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 class PlotlySankey extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.selectGbu = this.selectGbu.bind(this);
+    this.selectQuestion = this.selectQuestion.bind(this);
+    this.state = {
+      dropdownOpen: false,
+      gbu : "1",
+      question : 1
+    };
+  }
 
-    ////console.log(realData.sankey_sample["1"][0]);
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  selectGbu(event) {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      gbu: event.target.innerText
+    });
+  }
+
+  selectQuestion(event){
+
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+      question: event.target.innerText
+    });
+  }
+
+  filters(data){
 
     var source= [];
     var target= [];
     var value=  [];
-    realData.sankey_sample["1"].forEach(element => {
+
+
+
+    var filtered = [];
+    data[this.state.gbu].forEach(element => {
+      console.log(element.question);
+      if(element.question === this.state.question){
+        filtered.push(element);
+      }
+    });
+
+
+    return filtered;
+
+
+
+    // data.sankey_sample["1"].forEach(element => {
+    //   source.push(element.source);
+    //   target.push(element.target);
+    //   value.push(element.value);
+    // });
+
+
+    // return filtered;
+
+  }
+
+  render() {
+
+    var source= [];
+    var target= [];
+    var value=  [];
+
+    var filt = this.filters(sankeyData)
+
+    filt.forEach(element => {
       source.push(element.source);
       target.push(element.target);
       value.push(element.value);
     });
 
     var link = 
-      {source : source,
-      target: target,
-    value:value}
+      {
+        source : source,
+        target: target,
+        value:value
+      }
 
-    console.log(source);
+      
+    console.log(filt);
 
     return (
-      <Plot
-        data={[
-          {
-            type: "sankey",
-            orientation: "h",
-            node: {
-              pad: 15,
-              thickness: 30,
-              line: {
-                color: "black",
-                width: 0.5
-              },
-             label: sankeyData.label,
-             color: sankeyData.color
-                },
-            link: link
-          },
-        ]}
 
-        layout={{width:1500, height: 700, title: 'Sankey Sample'}}
-      />
+      <div>
+
+        <div>
+          <p>GBU: {this.state.gbu} Question: {this.state.question}</p>
+
+          <DropdownButton id="dropdown-basic-button" title="GBU">
+            <Dropdown.Item onClick={this.selectGbu}>1</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>2</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>3</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>4</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>5</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>6</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>7</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectGbu}>8</Dropdown.Item>
+          </DropdownButton>
+
+          <DropdownButton id="dropdown-basic-button" title="Question">
+            <Dropdown.Item onClick={this.selectQuestion}>1</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectQuestion}>2</Dropdown.Item>
+            <Dropdown.Item onClick={this.selectQuestion}>3</Dropdown.Item>
+          </DropdownButton>
+
+        </div>
+
+      <div>
+
+        
+        <Plot
+          data={[
+            {
+              type: "sankey",
+              orientation: "h",
+              node: {
+                pad: 15,
+                thickness: 30,
+                line: {
+                  color: "black",
+                  width: 0.5
+                },
+              label: realData.label,
+              color: realData.color
+                  },
+              link: link
+            },
+          ]}
+          layout={{width:1500, height: 700, title: 'Sankey Sample'}}
+        />
+        </div>
+      </div>
     );
   }
 }
--- src/components/PlotlySankey/realData.js
+++ src/components/PlotlySankey/realData.js
@@ -1,12 +1,12 @@
 export default {
 
+    label : ["Culture/Company", "Communication", "Bad", "Neutral", "Good"],
 
     sankey_sample : {"1": [{"source": 1, "value": 36, "target": 10, "question": '1'},
                         {"source": 1, "value": 5, "target": 9, "question": '1'},
                         {"source": 2, "value": 10, "target": 10, "question": '1'},
                         {"source": 3, "value": 12, "target": 9, "question": '1'},
                         {"source": 2, "value": 5, "target": 9, "question": '1'}]},
-
 
 
     source_map : {1: 'Culture/Company', 2: 'Communication', 3: 'Leadership', 4: 'Change',
