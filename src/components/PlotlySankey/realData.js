89ba09e68cdb719bf71853b0db59747cfbccb607
Anmol Arora <anmol.arora@atco.com>
2019-07-24 00:36:53 +0000

    updated real data for testing

--- src/components/PlotlySankey/realData.js
+++ src/components/PlotlySankey/realData.js
@@ -1,18 +1,18 @@
 export default {
 
 
-    sankey_sample : {"1": [{"source": 1, "value": 36, "target": -0, "question": '1'},
-                        {"source": 1, "value": 5, "target": -1, "question": '1'},
-                        {"source": 2, "value": 10, "target": -0, "question": '1'},
-                        {"source": 2, "value": 12, "target": -0.5, "question": '1'},
-                        {"source": 2, "value": 5, "target": -1, "question": '1'}]},
+    sankey_sample : {"1": [{"source": 1, "value": 36, "target": 10, "question": '1'},
+                        {"source": 1, "value": 5, "target": 9, "question": '1'},
+                        {"source": 2, "value": 10, "target": 10, "question": '1'},
+                        {"source": 3, "value": 12, "target": 9, "question": '1'},
+                        {"source": 2, "value": 5, "target": 9, "question": '1'}]},
 
 
 
     source_map : {1: 'Culture/Company', 2: 'Communication', 3: 'Leadership', 4: 'Change',
     5: 'Management', 6: 'Transparency', 7: 'Team', 8:'People/Employees'},
 
-    target_map : {0: 'Bad', 0.5: 'Neutral', 1: 'Good'}
+    target_map : {9: 'Bad', 10: 'Neutral', 11: 'Good'}
 
 
 }
